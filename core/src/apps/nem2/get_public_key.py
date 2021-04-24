241c3dd03e129b5cf09a0181cd206bbf9abbd249
Tri Vo <tri.vo@fdssoft.com>
2021-04-24 15:39:42 +0700

    Get nem2 public key a part (remove_ed25519_prefix)

--- core/src/apps/nem2/get_public_key.py
+++ core/src/apps/nem2/get_public_key.py
@@ -1,20 +1,47 @@
 from ubinascii import hexlify
 
-from trezor.messages.NEM2GetPublicKey import NEM2GetPublicKey
+from trezor import log, wire
 from trezor.messages.NEM2PublicKey import NEM2PublicKey
+from trezor.messages.HDNodeType import HDNodeType
 from trezor.ui.layouts import show_pubkey
 
 from apps.common import paths
 from apps.common.keychain import Keychain, auto_keychain
+from apps.common.seed import remove_ed25519_prefix
+
+
+if False:
+    from trezor.messages.NEM2GetPublicKey import NEM2GetPublicKey
 
 
 @auto_keychain(__name__)
-async def get_public_key(ctx, msg: NEM2GetPublicKey, keychain: Keychain):
-    await paths.validate_path(ctx, keychain, msg.address_n)
-    node = keychain.derive(msg.address_n)
-    pubkey = node.public_key()
+async def get_public_key(
+    ctx: wire.Context, msg: NEM2GetPublicKey, keychain: Keychain
+) -> NEM2PublicKey:
+    await paths.validate_path(
+        ctx,
+        keychain,
+        msg.address_n,
+    )
+
+    try:
+        key = _get_public_key(keychain, msg.address_n)
+    except ValueError as e:
+        if __debug__:
+            log.exception(__name__, e)
+        raise wire.ProcessError("Deriving public key failed")
 
     if msg.show_display:
-        await show_pubkey(ctx, hexlify(pubkey).decode())
+        await show_pubkey(ctx,  hexlify(key.public_key).decode())
+    return key
 
-    return NEM2PublicKey(public_key=pubkey)
+
+def _get_public_key(
+    keychain: Keychain, derivation_path: list[int]
+) -> NEM2PublicKey:
+    node = keychain.derive(derivation_path)
+    print("node.public_key()",node.public_key())
+    public_key = remove_ed25519_prefix(node.public_key())
+
+    return NEM2PublicKey(public_key=public_key)
+
